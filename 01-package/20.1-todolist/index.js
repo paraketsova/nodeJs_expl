b51960481319bdbee5516f1e4fad08a0cc0c2cef
Mariia Paraketsova <paraketsova@gmail.com>
2021-02-23 11:34:04 +0100

    Fix 19b-bjorn todo

--- 01-package/20.1-todolist/index.js
+++ 01-package/20.1-todolist/index.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
+var task = ["buy books", "practise with nodejs"];
 //placeholders for removed task
 var complete = ["finish jquery"];
 
