751bf707d8782d13f6b2e633216f53cb89bdb457
Mariia Paraketsova <paraketsova@gmail.com>
2021-02-24 12:05:53 +0100

    Update 19-todo-Micke

--- 01-package/19-todo-Micke/app.js
+++ 01-package/19-todo-Micke/app.js
@@ -4,30 +4,57 @@
 
 const express = require('express')
 const app = express()
-
-app.use('/public', express.static('assets'));
-
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session') // paket som innehåler cookies
+app.use('/public', express.static('assets')); // add allt som ligger i assets
+app.use(cookieSession({ // används för att kriptera
+    name: 'session',
+    keys: ['veryimportantsecret', 'notsoimportantsecret']
+}))
+app.use(bodyParser.urlencoded())
 
 app.get('/', (request, response) => {
-    let tasks = [
-        {
-            id: 1,
-            task: 'Tvätta kläder',
-            done: false
-        },
-        {
-            id: 2,
-            task: 'Handla',
-            done: false
-        },
-        {
-            id: 3,
-            task: 'Plugga node.js',
-            done: false
-        }
-    ];
-    response.render('index.ejs', { tasks: tasks })
+  let tasks = request.session.tasks || []  // egensk som finns i req.session.tasks spara i variabel, om det finns inte spara []
+  /*
+  let tasks = [
+    {
+      id: 1,
+      task: 'Tvätta kläder',
+      status: false
+    },
+     {
+      id: 2,
+      task: 'Handla',
+      status: false
+    },
+    {
+      id: 3,
+      task: 'plugga node.js',
+      status: false
+    }
+  ];*/
+  response.render('index.ejs', { tasks: tasks })
 })
+app.post('/add', (request, response) => {
+  let tasks = request.session.tasks || [] 
 
+  let max_value = 0
+  for (let task of tasks) {
+      if (task.id > max_value) {
+          max_value = task.id
+      }
+  }
+  max_value++
 
+  tasks.push({
+      id: max_value,
+      task: request.body.todo_item,
+      done: false
+  })
+
+  request.session.tasks = tasks
+  //console.log(request.body)
+  
+  response.redirect('/')
+})
 app.listen(3000)
