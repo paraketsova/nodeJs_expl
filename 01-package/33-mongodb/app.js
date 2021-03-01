0f5e85886873315c031c0135e9d7980a5198172e
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-01 11:33:35 +0100

    Update 33-mongodb

--- 01-package/33-mongodb/app.js
+++ 01-package/33-mongodb/app.js
@@ -1,27 +1,69 @@
-const { request } = require('express')
 const express = require('express')
 const app = express()
+
 const monk = require('monk')
-const db = monk('localhost:27017/nodetest1')  // från mongoDB compass host: ---
+const db = monk('localhost:27017/nodetest1')
+
+const bodyParser = require('body-parser')
+
 app.use((request, response, next) => {
-  request.db = db
-  next()
+    request.db = db
+    next()
 })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (request, response) => {
-  const users = request.db.get('user');
-  users
-      .find()
-      .then(data => {
-          console.log(data)
-          response.render('userlist.ejs', {users: data})
-      });
-});
+    const users = request.db.get('usercollection')
+    users
+        .find()
+        .then(data => {
+            response.render('userlist.ejs', { users: data })
+        })
+
+})
 
 app.post('/adduser', (request, response) => {
-  const users = request.db.get('user');
-  users.insert(request.body);
-  response.redirect('/');
-});
+    const users = request.db.get('usercollection')
 
+    users.insert(request.body)
+    response.redirect('/')
+})
 
-app.listen(3000);
+app.get('/edituser/:id', (request, response) => {
+    const users = request.db.get('usercollection')
+    users
+        .find({ _id: request.params.id })
+        .then(data => {
+            if (data.length == 1) {
+                response.render('edituser.ejs', data[0])
+            }
+            else {
+                response.redirect('/')
+            }
+        })
+})
+
+app.get('/removeuser/:id', (request, response) => {
+    const users = request.db.get('usercollection')
+
+    users.remove({ _id: request.params.id })
+    response.redirect('/')
+})
+
+app.post('/saveuser', (request, response) => {
+    const users = request.db.get('usercollection')
+    users.update(
+        {
+            _id: request.body.userid
+        }, {
+        $set: {
+            username: request.body.username,
+            email: request.body.email
+        }
+    })
+
+    response.redirect('/')
+})
+
+app.listen(3000)
--- 01-package/33-mongodb/dist/app.dev.js
+++ 01-package/33-mongodb/dist/app.dev.js
@@ -1,32 +1,64 @@
 "use strict";
 
-var _require = require('express'),
-    request = _require.request;
-
 var express = require('express');
 
 var app = express();
 
 var monk = require('monk');
 
-var db = monk('localhost:27017/nodetest1'); // från mongoDB compass host: ---
+var db = monk('localhost:27017/nodetest1');
+
+var bodyParser = require('body-parser');
 
 app.use(function (request, response, next) {
   request.db = db;
   next();
 });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.get('/', function (request, response) {
-  var users = request.db.get('user');
+  var users = request.db.get('usercollection');
   users.find().then(function (data) {
-    console.log(data);
     response.render('userlist.ejs', {
       users: data
     });
   });
 });
 app.post('/adduser', function (request, response) {
-  var users = request.db.get('user');
+  var users = request.db.get('usercollection');
   users.insert(request.body);
+  response.redirect('/');
+});
+app.get('/edituser/:id', function (request, response) {
+  var users = request.db.get('usercollection');
+  users.find({
+    _id: request.params.id
+  }).then(function (data) {
+    if (data.length == 1) {
+      response.render('edituser.ejs', data[0]);
+    } else {
+      response.redirect('/');
+    }
+  });
+});
+app.get('/removeuser/:id', function (request, response) {
+  var users = request.db.get('usercollection');
+  users.remove({
+    _id: request.params.id
+  });
+  response.redirect('/');
+});
+app.post('/saveuser', function (request, response) {
+  var users = request.db.get('usercollection');
+  users.update({
+    _id: request.body.userid
+  }, {
+    $set: {
+      username: request.body.username,
+      email: request.body.email
+    }
+  });
   response.redirect('/');
 });
 app.listen(3000);
