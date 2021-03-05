1b3c41e94d989c7270a39988a8259ef5657a6bcc
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-05 10:27:14 +0100

    Add in 01-package 38-passport

---
+++ 01-package/38-passport/dist/app.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var passport = require('passport');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+app.post('login', passport.authenticate('local', {
+  // фактически это вариация app.post (request, response)
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+passport.use(new LocalStrategy( // модульный вариант app.use где мы прописываем что будем делать в зависимости от ввода пароля и имени юзера
+{
+  usernameField: 'email',
+  passwordField: 'passwd'
+}, function (username, password, done) {
+  //в скобках значение юзернейм, полученное от датабазы
+  username.findOne({
+    username: username
+  }, function (error, user) {
+    if (error) {
+      return done(error);
+    }
+
+    if (!user) {
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+
+    if (!user.validPassport(password)) {
+      return done(null, false, {
+        message: "Incorrect password. "
+      });
+    }
+    /*       if(!user.validPassport(password)) {
+            return done(null, false, { message: "Incorrect password or user. " })
+          }  */
+
+  });
+}));
