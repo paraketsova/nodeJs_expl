dc0b22b7038c23daf0383569eca9b81c212ca561
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-10 09:57:23 +0100

    Update 37, 37.1, 40

--- 01-package/40-books_api/app.js
+++ 01-package/40-books_api/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));// тру означает что мы можем постить нестед объекты
+app.use(express.json())
+
+let books = []
+
 app.get('/', (request, response) => {
-  response.end('<h1>Hello</h1>');
+    response.end('<h1>Hello</h1>')
 })
 
-app.listen(3000);
+app.post('/book', (request, response) => {
+    const book = request.body
+
+    console.log(book)
+    books.push(book)
+
+    response.end('Book was added')
+})
+
+
+
+app.listen(3000)
