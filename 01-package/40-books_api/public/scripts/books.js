64edaaf74abed65ff4501f3fabb51fb95457eb8b
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-10 10:55:10 +0100

    Update 40 (add book-list and JSON for test)

--- 01-package/40-books_api/public/scripts/books.js
+++ 01-package/40-books_api/public/scripts/books.js
@@ -1,13 +1,22 @@
-const { response } = require("express")
-
 const loadBooks = () => {
-  const books = fetch('/books')
-    .then(response => {
-      response.json()//kontrollera statuskod?
-    })
-    .then(data => {
-      console.log(data) //browser console log
-    })
-};
+  fetch('/books')
+      .then(response => response.json())
+      .then(books => {
+          let book_list = document.getElementById('books')
 
-loadBooks();
+          for (let book of books) {
+              let book_element = `
+              <div class="card" style="width: 18rem;">
+                  <div class="card-body">
+                      <h5 class="card-title">${book.title}</h5>
+                      <p class="card-text">${book.description}</p>
+                      <a href="#" class="btn btn-primary">View details</a>
+                  </div>
+              </div>`
+
+              book_list.innerHTML += book_element
+          }
+      })
+}
+
+loadBooks()
