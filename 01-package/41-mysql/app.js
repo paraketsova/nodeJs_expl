c2d0615f227ee6f47434433761b5e7602c3501fa
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-12 11:19:57 +0100

    Update 41-mysql

--- 01-package/41-mysql/app.js
+++ 01-package/41-mysql/app.js
@@ -15,21 +15,57 @@ const connection = mysql.createConnection({
 })
 
 connection.connect(error => {
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+      console.log(error)
+  }
 
-    console.log('Connected')
+  console.log('Connected')
 })
 
 app.get('/users', (request, response) => {
-    connection.query('SELECT * FROM employees', (error, users) => {
-        if (error) {
-            throw error;
-        }
-        response.render('users', { users: users })
-    })
+  connection.query('SELECT * FROM employees', (error, users) => {
+      if (error) {
+          throw error;
+      }
+      response.render('users', { users: users })
+  })
 })
+
+app.get('/api/users', (request, response) => {
+  connection.query('SELECT employeeNumber, lastName, firstName, email FROM employees', (error, users) => {
+      if (error) {
+          throw error;
+      }
+      response.json(users)
+  })
+})
+
+/*
+const employee = { firstName: 'Winnie', officeCode: 1 };
+connection.query('INSERT INTO employees (employeeNumber, firstName) VALUES (?, ?)', ['12345', 'Winnie'], (err, result) => {
+  if (err) throw err;
+
+  console.log('Last insert ID:', result.insertId);
+});
+
+connection.query(
+  'UPDATE employees SET location = ? WHERE ID = ?',
+  ['South Africa', 5],
+  (err, result) => {
+      if (err) throw err;
+
+      console.log(`Changed ${result.changedRows} row(s)`);
+  }
+);
+
+connection.query(
+  'DELETE FROM employees WHERE id = ?', [5], (err, result) => {
+      if (err) throw err;
+
+      console.log(`Deleted ${result.affectedRows} row(s)`);
+  }
+);
+*/
 
 
 app.listen(3000)
