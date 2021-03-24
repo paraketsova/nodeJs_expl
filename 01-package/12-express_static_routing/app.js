3507830061912bcf436f8cdaab8ebdf4980d9d52
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-25 00:39:51 +0100

    Update

--- 01-package/12-express_static_routing/app.js
+++ 01-package/12-express_static_routing/app.js
@@ -8,11 +8,11 @@ app.get('/', (request, response) => {
     response.sendFile('./landing-page/home.html', { root: __dirname });
 });
 
-app.get('/about', (request, response) => {
-    response.sendFile('./landing-page/about.html', { root: __dirname });
+app.get('/about', (request, response) => { //если в адресной строке localhost:3000/assets/about
+    response.sendFile('./landing-page/about.html', { root: __dirname }); // получаем файл about.html, лежащий где указано
 });
 
-app.get('/contact', (request, response) => {
+app.get('/contact', (request, response) => {  //если в адресной строке localhost:3000/assets/contact
     response.sendFile('./landing-page/contact.html', { root: __dirname });
 });
 
